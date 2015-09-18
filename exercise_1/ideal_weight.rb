77d1ece9aa0e58b460624fb6cd6bd534f831335a
sbolshakov <stepan.bolshakov@gmail.com>
2015-09-18 13:52:44 +0300

    Update ideal_weight.rb
    Тип переменно heights изменен на Float

--- exercise_1/ideal_weight.rb
+++ exercise_1/ideal_weight.rb
@@ -1,5 +1,5 @@
 print "Представьтесь, пожалуйста: "
 name = gets.chomp
 print "Какой у вас рост в сантиметрах? "
-heights = gets.chomp.to_i
+heights = gets.chomp.to_f
 puts "#{name}, ваш идеальный вес: #{heights-110} кг"
