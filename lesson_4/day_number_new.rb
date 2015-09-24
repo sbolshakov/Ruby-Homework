01c1b740ccfd388990fd38b2598af1c71e23225e
Stepan Bolshakov <stepan.bolshakov@gmail.com>
2015-09-24 20:05:43 +0300

    Урок 4. Номер дня v3

--- lesson_4/day_number_new.rb
+++ lesson_4/day_number_new.rb
@@ -8,9 +8,7 @@ year = gets.chomp.to_i
 days_in_month = {0 => 0, 1 => 31, 2 => 28, 3 => 31, 4 => 30, 5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
 result = day
 
-(1..month).each do | mon |
- result += days_in_month[mon-1]
-end
+(1..month).each { | mon | result += days_in_month[mon-1] }
 
 # Проверяем високосный год (делится на 4 и не делится на 100 или делится на 400)
 
